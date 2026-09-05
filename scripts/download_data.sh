28f573b3d3d0442cb7071bb62f01bcc26cd2e3c5
George Perdrizet <george@perdrizet.org>
2026-09-05 03:36:50 +0000

    Moved input data to data/raw/

--- scripts/download_data.sh
+++ scripts/download_data.sh
@@ -1,4 +1,4 @@
 #/bin/bash
 
-kaggle competitions download -c rsna-knee-abnormality-detection -p data
-unzip data/rsna-knee-abnormality-detection.zip -d data
+kaggle competitions download -c rsna-knee-abnormality-detection -p data/raw
+unzip data/raw/rsna-knee-abnormality-detection.zip -d data/raw
