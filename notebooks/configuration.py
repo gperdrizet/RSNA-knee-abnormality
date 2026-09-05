b8dd6b7eba0fdc89872ca37cb33586063c93c809
George Perdrizet <george@perdrizet.org>
2026-09-05 20:33:31 +0000

    Switched to relative paths for notebook configuration

--- notebooks/configuration.py
+++ notebooks/configuration.py
@@ -2,12 +2,9 @@
 Configuration file for notebook paths and settings.
 '''
 
-import os
-
-
 # Raw data paths
-DATA_DIR         = os.path.join('data', 'raw')
-TRAIN_CSV        = os.path.join(DATA_DIR, 'train.csv')
-TRAIN_SERIES_CSV = os.path.join(DATA_DIR, 'train_series.csv')
-TEST_CSV         = os.path.join(DATA_DIR, 'test.csv')
-TEST_SERIES_CSV  = os.path.join(DATA_DIR, 'test_series.csv')
+DATA_DIR         = '../data/raw'
+TRAIN_CSV        = f'{DATA_DIR}/train.csv'
+TRAIN_SERIES_CSV = f'{DATA_DIR}/train_series.csv'
+TEST_CSV         = f'{DATA_DIR}/test.csv'
+TEST_SERIES_CSV  = f'{DATA_DIR}/test_series.csv'
