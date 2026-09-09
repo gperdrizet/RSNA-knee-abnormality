116268b397ebf99c6ff038906507022c9175077d
George Perdrizet <george@perdrizet.org>
2026-09-09 03:50:16 +0000

    Add repo root to sys path

--- notebooks/configuration.py
+++ notebooks/configuration.py
@@ -1,6 +1,12 @@
 '''
 Configuration file for notebook paths and settings.
 '''
+import sys
+from pathlib import Path
+
+# Make the repo root importable
+REPO = Path(__file__).resolve().parents[1]
+sys.path.insert(0, str(REPO))
 
 # Raw data paths
 DATA_DIR         = '../data/raw'
